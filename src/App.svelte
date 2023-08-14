c1f3cd1dd1603443dd5487867d88f415ad9d2b0f
Robinho1997 <robin.kempen@gmx.de>
2023-08-14 23:34:41 +0200

    src code

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+    import {Router,Route} from "svelte-routing";
+    import Navbar from "./components/Navbar.svelte";
+    import Home from "./components/Home.svelte";
+    import About from "./components/About.svelte";
+    import Contact from "./components/Contact.svelte";
+</script>
+
+<Navbar/>
+
+<Router>
+    <Route component={Home}/>
+    <Route path="/about">
+        <About />
+    </Route>
+    <Route path="contact">
+        <Contact/>
+    </Route>
+</Router>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,28 @@
+<nav>
+    <ul>
+        <li> <a href="/">Home</a> </li>
+        <li> <a href="/about">About</a> </li>
+        <li> <a href="/contact">Contact</a> </li>
+    </ul>
+</nav>
+
+<style>
+
+    ul {
+        display: flex;
+        justify-content: space-between;
+        padding: 5px 15px;
+        margin: 0;
+        background-color: orange;
+    }
+
+    li {
+        list-style: none;
+        font-size: 20px;
+    }
+
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+</style>
